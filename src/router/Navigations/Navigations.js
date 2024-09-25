8bcd9d4f2d98ff797d2bce4a2dc43590865e2604
katerynalanina <lanina.kateryna@gmail.com>
2024-09-25 16:54:51 +0300

    feat: remove text on pages

--- src/router/Navigations/Navigations.js
+++ src/router/Navigations/Navigations.js
@@ -31,7 +31,6 @@ const Navigations = () => {
       {windowWidth > 768 && windowWidth < 1024 ?
         <NavLink to="/popMovies"><MdFlutterDash size={'24'}/>Popular Movie</NavLink> :null}
       <p>popular genders</p>
-      <p>menu</p>
       <NavLink to="/genre/action"><GiPistolGun size={'24'}/>Action</NavLink>
       <NavLink to="/genre/adventure"><SiOpenstreetmap size={'24'}/>Adventure</NavLink>
       <NavLink to="/genre/comedy"><BsEmojiSmile size={'24'}/>Comedy</NavLink>
